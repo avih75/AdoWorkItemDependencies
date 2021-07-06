185b55ae9494d77a56eea3d54001747c8c6e5789
avi <avih75@gmail.com>
2021-07-06 16:24:34 +0300

    build extenshion

--- scripts/dependenciesControl.ts
+++ scripts/dependenciesControl.ts
@@ -1,25 +1,11 @@
 import { CreateView, TryChangeValue } from "./dependenciesView";
 export class Controller {
     constructor(id: number) {
-        let inputs = VSS.getConfiguration().witInputs;
-        let ApprovelStateField = inputs["ApprovelStateField"];
-        let ValueToApproveField = inputs["ValueToApproveField"];
-        let ApprovedValueField = inputs["ApprovedValueField"];
-        let LastRequested = inputs["LastRequested"];
-        let LastRequestState = inputs["LastRequestState"];
-        let ApproveBy = inputs["ApproveBy"];
-        let ApproveValueCondition = inputs["ApproveValueCondition"];
-        let ApproveCondition = inputs["ApproveCondition"];
-        let ApprovedPreviusApprove = inputs["ApprovedPreviusApprove"];
-        let ApprovedComment = inputs["ApprovedComment"];
-        let Approver = inputs["Approver"];
-        let TrigerNeedApproveState = inputs["TrigerNeedApproveState"];
-        let BlockField = inputs["BlockField"];
-        if (!ApprovedComment)
-            ApprovedComment = "";
-        CreateView(ApprovelStateField, ValueToApproveField, ApprovedValueField, ApproveBy, ApproveValueCondition,
-            ApproveCondition, ApprovedPreviusApprove, ApprovedComment, LastRequested, LastRequestState, Approver,
-            BlockField, TrigerNeedApproveState);
+        let inputs = VSS.getConfiguration().witInputs; 
+        let ListBlockedState = inputs["ListBlockedState"];
+        let RelBlocekd = inputs["RelBlocekd"];
+        let DependOnState = inputs["DependOnState"];
+        CreateView(DependOnState, ListBlockedState, RelBlocekd)
     }
     public FieldChanged(changedFields: { [key: string]: any; }) {
         TryChangeValue(changedFields);
